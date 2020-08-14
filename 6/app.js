2f9154f0b45c80c428d0c875e8a22496fe22c18b
Syahir <888syahir@gmail.com>
2020-08-15 02:33:33 +0800

    keyboard events lesson

---
+++ 6/app.js
@@ -0,0 +1,13 @@
+new Vue({
+	el: '#vue-app',
+	data: {},
+	methods: {
+		logName: function () {
+			console.log("You've enter your name");
+		},
+
+		logAge: function () {
+			console.log("You've enter your age");
+		},
+	},
+});
