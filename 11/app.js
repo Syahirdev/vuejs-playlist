69550feda3776913ed7a4a4bf8f56669d1ea284c
Syahir <888syahir@gmail.com>
2020-08-15 04:10:05 +0800

    looping lesson

---
+++ 11/app.js
@@ -0,0 +1,13 @@
+new Vue({
+	el: '#vue-app',
+	data: {
+		characters: ['Syahir', 'Syazmi', 'Syakira'],
+		persons: [
+			{ name: 'Along', age: 34 },
+			{ name: 'Angah', age: 26 },
+			{ name: 'Acu', age: 23 },
+		],
+	},
+	methods: {},
+	computed: {},
+});
