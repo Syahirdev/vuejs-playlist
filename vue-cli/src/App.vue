306a013ebd804af59d13844b605289146142d4e3
Syahir <888syahir@gmail.com>
2020-08-16 00:55:40 +0800

    slots
    simple slot implementation to understand how slot works :D

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,42 @@
 <template>
-	<div></div>
+	<div>
+		<form-helper>
+			<div slot="form-header">
+				<h3>This is the title of a form</h3>
+				<p>This is some info about the form</p>
+			</div>
+			<div slot="form-fields">
+				<input type="text" placeholder="name" required /><br />
+				<input type="password" placeholder="password" required />
+			</div>
+			<div slot="form-controls">
+				<button v-on:click="handleSubmit">Submit</button>
+			</div>
+		</form-helper>
+	</div>
 </template>
 
 <script>
+// Imports
+import formHelper from './components/formHelper.vue';
 export default {
+	components: {
+		'form-helper': formHelper,
+	},
 	data() {
 		return {};
 	},
-	components: {},
-	methods: {},
+	methods: {
+		handleSubmit: function() {
+			alert('thanks for submitting');
+		},
+	},
 };
 </script>
 
-<style></style>
+<style>
+body {
+	margin: 0;
+	font-family: 'Nunito SemiBold';
+}
+</style>
