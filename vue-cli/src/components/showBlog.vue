afd6d3a8845f21eacb9b767ab510b655f0d978b7
Syahir <888syahir@gmail.com>
2020-08-16 04:36:30 +0800

    add seaarch box using custom filter

--- vue-cli/src/components/showBlog.vue
+++ vue-cli/src/components/showBlog.vue
@@ -1,7 +1,8 @@
 <template>
 	<div id="show-blogs">
 		<h1>All Blog Articles</h1>
-		<div v-for="blog in blogs" :key="blog.id" class="single-blog">
+		<input type="text" v-model="search" placeholder="Search..." />
+		<div v-for="blog in filteredBlogs" :key="blog.id" class="single-blog">
 			<h2 v-rainbow>{{ blog.title | toUpperCase }}</h2>
 			<article>{{ blog.body | snippet }}</article>
 		</div>
@@ -15,6 +16,7 @@ export default {
 	data() {
 		return {
 			blogs: [],
+			search: '',
 		};
 	},
 	components: {},
@@ -23,6 +25,13 @@ export default {
 		axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
 			this.blogs = response.data.slice(0, 10);
 		});
+	},
+	computed: {
+		filteredBlogs: function() {
+			return this.blogs.filter((blog) => {
+				return blog.title.toLowerCase().match(this.search.toLowerCase());
+			});
+		},
 	},
 };
 </script>
