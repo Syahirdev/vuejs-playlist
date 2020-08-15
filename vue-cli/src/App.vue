2e0cc6cfb68cc837c8f26f77af58f9f1aa54b692
Syahir <888syahir@gmail.com>
2020-08-15 23:47:41 +0800

    Revert to Vue-CLI
    @.@

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,29 +1,10 @@
 <template>
-	<div>
-		<keep-alive>
-			<component v-bind:is="component"></component>
-		</keep-alive>
-		<button @click="component = 'form-one'">Show Form One</button>
-		<button @click="component = 'form-two'">Show Form Two</button>
-	</div>
+	<div></div>
 </template>
 
 <script>
-import formOne from './components/formOne.vue';
-import formTwo from './components/formTwo.vue';
 export default {
-	components: {
-		'form-one': formOne,
-		'form-two': formTwo,
-	},
-	data() {
-		return {
-			component: 'form-one',
-		};
-	},
-	methods: {
-		handleSubmit: function() {},
-	},
+	components: {},
 };
 </script>
 
