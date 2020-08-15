1fc9d9cd094b371cefecd0ca38c407b0c0289ad3
Syahir <888syahir@gmail.com>
2020-08-15 20:18:52 +0800

    clean the scaffold and insert Persons component

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<h1>{{ title }}</h1>
+		<persons></persons>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Persons from './Persons.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	components: {
+		persons: Persons,
+	},
+	data() {
+		return {
+			title: 'My First App!',
+		};
+	},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ vue-cli/src/Persons.vue
@@ -0,0 +1,17 @@
+<template>
+	<ul>
+		<li v-for="person in persons" :key="person.id">{{ person }}</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			persons: ['Syahir', 'Syazmi', 'Syakira'],
+		};
+	},
+};
+</script>
+
+<style></style>
